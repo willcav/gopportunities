d0464f38c4ebe3644e755f421dfe22e2509794df
willcav <willcavenagli@gmail.com>
2024-04-07 17:35:36 -0300

    feat: add database handler

--- config/config.go
+++ config/config.go
@@ -1,7 +1,27 @@
 package config
 
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
 func Init() error {
+	var err error
+	// Initialize SQLite
+	db, err = InitializeSQLite()
+	if err != nil {
+		return fmt.Errorf("Error initializing sqlite: %v", err)
+	}
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
---
+++ config/sqlite.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+
+	"github.com/willcav/gopportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+	// Check if db file exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("Database file not found, creating...")
+
+		// Create the database file and directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+
+		file.Close()
+	}
+
+	// Create DB and connect
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	// Migrate the schema
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
