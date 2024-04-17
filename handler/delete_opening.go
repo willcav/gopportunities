a17d23c12c1914b83802da0b6e20201cc7552f89
willcav <willcavenagli@gmail.com>
2024-04-17 02:23:19 -0300

    docs: add documentation swagger

--- handler/delete_opening.go
+++ handler/delete_opening.go
@@ -8,6 +8,18 @@ import (
 	"github.com/willcav/gopportunities/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Delete opening
+// @Description Delete a job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening ID"
+// @Success 200 {object} CreateOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Router /opening [delete]
 func DeleteOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
--- handler/response.go
+++ handler/response.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/willcav/gopportunities/schemas"
 )
 
 func sendError(ctx *gin.Context, code int, msg string) {
@@ -21,4 +22,14 @@ func sendSuccess(ctx *gin.Context, op string, data interface{}) {
 		"message": fmt.Sprintf("Operation from handler: %s successful", op),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode string `json:"errorCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
